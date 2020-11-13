4166fe5af5484a71b32fba7dbe05d66dd9333684
Tony Pulickal <ttp@hashicorp.com>
2020-11-12 21:16:28 -0500

    add generated templates and overrides

---
+++ cleanup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# setup environment
+#Adjust this to where YAML templates are
+manifests_dir=$(pwd)/consul-helm/manifests/consul/templates 
+
+if [ ! -d $manifests_dir ]; then
+  echo "Manifests directory does not exist: $manifests_dir"
+  echo "Please adjust manifests_dir directory variable"
+  exit
+fi
+
+cli=oc #Set this to oc or kubectl
+
+echo "Deleting objects"
+pushd ${manifests_dir}
+$cli delete -f server
+$cli delete -f consul-server-scc.yaml
+$cli delete -f client
+$cli delete -f consul-client-scc.yaml
+$cli delete -f sync-catalog/
+$cli delete -f connect-inject/
+$cli delete -f dns-service.yaml
+$cli delete -f ui-service.yaml
+#$cli delete pvc/data-default-consul-oc-consul-server-0
+#$cli delete pvc/data-default-consul-oc-consul-server-1
+#$cli delete pvc/data-default-consul-oc-consul-server-2
+popd
+
+echo "Deleting example application"
+$cli delete -f services/
+
+echo "Done, running $cli get pods, please verify there are no consul or application pods"
+$cli get pods
